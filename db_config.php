17a09f50e474e45f829c9001c4c238abd93c06ca
Sakthi10122004 <sakthikaribeeran@gmail.com>
2026-04-30 12:01:04 +0530

    Handle MySQLi exceptions gracefully to prevent 500 errors

--- db_config.php
+++ db_config.php
@@ -4,8 +4,10 @@ $db = 'exam_cell_automation';
 $user = 'root';
 $pass = '';
 
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try {
+    $conn = new mysqli($host, $user, $pass, $db);
+} catch (mysqli_sql_exception $e) {
+    die("<h3>Database Connection Failed!</h3><p>" . $e->getMessage() . "</p><p><b>Please update db_config.php with your InfinityFree MySQL credentials!</b></p>");
 }
 ?>
