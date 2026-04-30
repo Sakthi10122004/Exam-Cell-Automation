3be53b66a9a915ff18764fc8ae9db069902824b5
Sakthi10122004 <sakthikaribeeran@gmail.com>
2026-04-30 12:14:30 +0530

    Add try/catch to database setup script to show SQL errors

--- setup_database.php
+++ setup_database.php
@@ -12,20 +12,21 @@ if (!file_exists($sql_file)) {
 // Read the SQL file
 $sql = file_get_contents($sql_file);
 
-// Execute multi query
-if ($conn->multi_query($sql)) {
-    do {
-        // Store first result set
-        if ($result = $conn->store_result()) {
-            $result->free();
-        }
-        // Prepare next
-    } while ($conn->more_results() && $conn->next_result());
-    
-    echo "<p style='color: green;'>Database tables created successfully from db.sql!</p>";
-    echo "<p>Please delete this setup_database.php file for security reasons.</p>";
-} else {
-    echo "<p style='color: red;'>Error creating tables: " . $conn->error . "</p>";
+try {
+    if ($conn->multi_query($sql)) {
+        do {
+            if ($result = $conn->store_result()) {
+                $result->free();
+            }
+        } while ($conn->more_results() && $conn->next_result());
+        
+        echo "<p style='color: green;'>Database tables created successfully from db.sql!</p>";
+        echo "<p>Please delete this setup_database.php file for security reasons.</p>";
+    } else {
+        echo "<p style='color: red;'>Error creating tables: " . $conn->error . "</p>";
+    }
+} catch (Exception $e) {
+    echo "<p style='color: red;'>SQL Error occurred: " . $e->getMessage() . "</p>";
 }
 
 $conn->close();
